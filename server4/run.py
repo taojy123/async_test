7510242de6437b1d2b42dfe7f25d8f6c38914ba2
root <taojy123@163.com>
2019-12-21 15:06:36 +0800

    update sanic testcase

--- server4/run.py
+++ server4/run.py
@@ -1,22 +1,24 @@
 
 from sanic import Sanic
 from sanic.response import json
-import requests
-import threading
-
-print(threading.active_count())
+import aiohttp
 
 app = Sanic()
 
 @app.route("/")
 async def test(request):
     url = 'http://lovegaudi.art:3000'
-    requests.get(url)
-    print(threading.active_count())
-    return json({"hello": "world"})
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            text = await resp.text()
+    return json({"hello": text})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
+# python3.7
+# pip install sanic aiohttp
 # python run.py
 
